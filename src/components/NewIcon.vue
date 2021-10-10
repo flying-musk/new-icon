9cb11eb8b8bdd3a01a481bcc3f4892134f0cec00
Flying Musk <musk.flying@gmail.com>
2021-10-10 22:18:31 +0800

    [new-icon] Initialize project: eslint rule and main component.

---
+++ src/components/NewIcon.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="n-outer">Hi NewIcon</div>
+</template>
+
+<script>
+export default {
+  name: 'NewIcon',
+};
+</script>
+
+<style lang="scss" scoped>
+.n-outer {
+  border: solid 1px #cccc00;
+}
+</style>
