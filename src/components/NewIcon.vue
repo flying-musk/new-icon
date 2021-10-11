e4231d4c3de68cc94cecc2a123a56c6fd1676b88
Flying Musk <musk.flying@gmail.com>
2021-10-12 01:08:22 +0800

    [new-icon] Implement function to buttons and add answer feedback.

--- src/components/NewIcon.vue
+++ src/components/NewIcon.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="n-outer">
+    <div :class="['n-veil', 'n-veil-correct', veilCorrect]"></div>
+    <div :class="['n-veil', 'n-veil-incorrect', veilIncorrect]"></div>
     <h1>New Icon</h1>
     <h2>Please identify the new icons.</h2>
     <p>
@@ -11,8 +13,8 @@
     </div>
     <div class="n-space"></div>
     <div class="n-buttons">
-      <button>Old</button>
-      <button>New</button>
+      <button @click="buttonClick(false)">Old</button>
+      <button @click="buttonClick(true)">New</button>
     </div>
   </div>
 </template>
@@ -23,11 +25,14 @@ export default {
   name: 'NewIcon',
   data() {
     return {
+      ICON_AMOUNT: 120,
+      veilCorrect: '',
+      veilIncorrect: '',
       correctCount: 0,
       incorrectCount: 0,
-      iconIndex: 0,
+      currentIconIndex: 0,
       iconSource: FONT_AWESOME_ICONS.sort(() => Math.random() - 0.5)
-        .slice(0, 120)
+        .slice(0, this.ICON_AMOUNT)
         .map((icon, idx) => ({ idx: idx, icon: icon, new: true })),
     };
   },
@@ -36,7 +41,27 @@ export default {
       return this.correctCount - this.incorrectCount;
     },
     icon: function () {
-      return this.iconSource[this.iconIndex].icon;
+      return this.iconSource[this.currentIconIndex].icon;
+    },
+  },
+  methods: {
+    buttonClick(isNew) {
+      if (isNew === this.iconSource[this.currentIconIndex].new) {
+        this.correctCount++;
+        this.veilCorrect = 'n-veil-show';
+        setTimeout(() => {
+          this.veilCorrect = '';
+        }, 200);
+      } else {
+        this.incorrectCount++;
+        this.veilIncorrect = 'n-veil-show';
+        setTimeout(() => {
+          this.veilIncorrect = '';
+        }, 200);
+      }
+      this.iconSource[this.currentIconIndex].new = false;
+      this.currentIconIndex =
+        Math.trunc(Math.random() * 100000000) % this.ICON_AMOUNT;
     },
   },
 };
@@ -46,6 +71,33 @@ export default {
 .n-outer {
   display: flex;
   flex-direction: column;
+}
+.n-veil {
+  z-index: -1;
+  opacity: 0;
+  transition: opacity 0.4s ease-out;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+}
+.n-veil-correct {
+  background: linear-gradient(
+    180deg,
+    rgb(16, 255, 16) 0%,
+    rgb(255, 255, 255) 50%
+  );
+}
+.n-veil-incorrect {
+  background: linear-gradient(
+    180deg,
+    rgb(204, 0, 0) 0%,
+    rgb(255, 255, 255) 50%
+  );
+}
+.n-veil-show {
+  opacity: 1;
 }
 .n-icon {
   align-self: center;
